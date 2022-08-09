3c459819ee2502a9b99323dc16cbb7e5f0911fc3
ossamalouati <92301144+OssamaLouati@users.noreply.github.com>
2022-08-09 16:50:56 +0100

    finish setting the playlist

--- src/playlist.js
+++ src/playlist.js
@@ -26,183 +26,235 @@ import cover from './assets/images/cover.jpg';
 function chillHop() {
   return [
     {
-      name: "Oasis",
+      name: "3AYCHIN MOLOUK",
       cover: cover ,
-      artist: "Makzo",
+      artist: "Ultras Bleu Ines",
       audio: song1,
       color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: true,
     },
+    
     {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
+      name: "F LA CURVA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song2,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
+      name: "EMISTE YA DEMDOUMA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song3,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Keep Going",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
+      name: "NSINI F DENYA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song4,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Going Back",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10310",
-      color: ["#335561", "#3A393E"],
+      name: "NIKO L INSEA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song5,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Bliss",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
-      artist: "Misha, Jussi Halme",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
-      color: ["#2A416D", "#E98087"],
+      name: "MI CORAZON",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song6,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Growing Apart",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9219",
-      color: ["#BD3D76", "#551853"],
+      name: "RYE7 YA L BNAY",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song7,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Sails",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
-      artist: "Strehlow, Aylior",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9355",
-      color: ["#F2C5AB", "#BE5853"],
+      name: "ZER9A MI AMORE",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song8,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Cruisin'",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/8404541e3b694d16fd79433b142ed910f36764dd-1024x1024.jpg",
-      artist: "Cloudchord, G Mills",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8200",
-      color: ["#FF194A", "#41B3B3"],
+      name: "LYUM KI L3ADA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song9,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Maple Leaf Pt.2",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
-      artist: "Philanthrope",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10243",
-      color: ["#CA483B", "#682C24"],
+      name: "SIAMO L INPT",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song10,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
+      name: "BI MECORASSON",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song11,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
+      name: "COURAGE",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song12,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Leaving For Good",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/7a84488fd87082302cb69c05262f2f3f87e93018-1024x1024.jpg",
-      artist: "Hanz",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8264",
-      color: ["#90B4AA", "#F2AE87"],
+      name: "ZER9A W L BAYDA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song13,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Eastway",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/c572841e8431cebc120dffed4f92119f723dd954-1024x1024.jpg",
-      artist: "Dontcry, Nokiaa",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9071",
-      color: ["#B442A1", "#F54F7A"],
+      name: "OBRIGADO CAMPIONE",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song14,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Wake up",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
-      artist: "Evil Needle",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8285",
-      color: ["#A35CA0", "#EE8D66"],
+      name: "VAMOS",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song15,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
+      name: "JINAKOM JINA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song16,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Velocities",
-      cover: "https://i.scdn.co/image/ab67616d0000b2734fb6a52430e65dbc6c593faf",
-      artist: "Sleepy Fish",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=3524",
-      color: ["#74c7b4", "#1d6d92"],
+      name: "UNIDOS",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song17,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
     {
-      name: "Deeper",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
-      artist: "Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10460",
-      color: ["#C668B2", "#4D92D7"],
+      name: "SANS ARRET",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song18,
+      color: ["#06424D", "#A1C0EE"],
       id: uuidv4(),
       active: false,
     },
+    
+    {
+      name: "LA STORIA GRANDE",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song19,
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+    
+    {
+      name: "HONORE LBANDERA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song20,
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+    
+    {
+      name: "NASSI DENYA",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song21,
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+    
+    {
+      name: "CRAZY PUBLICO",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song22,
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+    
+    {
+      name: "SENZA FINE",
+      cover: cover ,
+      artist: "Ultras Bleu Ines",
+      audio: song23,
+      color: ["#06424D", "#A1C0EE"],
+      id: uuidv4(),
+      active: false,
+    },
+    
 
     //ADD MORE HERE
   ];
