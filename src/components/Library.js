110adada7a6ba166226f3fa1325ba792261a720b
Oussama Louati <louati.oussama@ine.inpt.ma>
2022-09-15 18:34:53 -0700

    finish new song text

--- src/components/Library.js
+++ src/components/Library.js
@@ -11,7 +11,7 @@ const Library = ({
 }) => {
   return (
     <div className={`library ${libraryStatus ? "active-library" : ""} `}>
-      <h2>Library</h2>
+      <h2>Library<span>NEW SONG</span></h2>
       <div className="library-songs">
         {songs.map((song) => (
           <LibrarySong
